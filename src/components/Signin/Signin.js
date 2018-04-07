dfd55474cf2576d2eeab7de001e54c7e9ab946bf
mi5ul <mirt.suljic@gmail.com>
2018-04-07 15:24:29 +1200

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitChange = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://pacific-badlands-66820.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
